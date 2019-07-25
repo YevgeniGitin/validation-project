df371599632d703dfd1140086728695e7599750a
Yevgeni Gitin <yevgeni.gitin21@gmail.com>
2019-07-25 18:30:34 +0300

    fixed commit

--- index1.js
+++ index1.js
@@ -1,29 +1,33 @@
 function start() {
     document.querySelector("#name").addEventListener("keyup", function(event) {
-        if (event.keyCode === 13)
+        if (event.keyCode === 13) {
             document.querySelector("#check").click();
+        }
     });
     document.querySelector("#address").addEventListener("keyup", function(event) {
-        if (event.keyCode === 13)
+        if (event.keyCode === 13) {
             document.querySelector("#check").click();
+        }
     });
 }
 
 function check(event) {
-    let nameflag = 0;
-    let addressflag = 0;
+    let nameflag = false;
     let res = "";
     if (document.querySelector("#name").value.length < 5) {
-        nameflag = 1;
+        nameflag = true;
         res += "Name should be more than 5 characters";
     }
     if (document.querySelector("#address").value.length > 9) {
-        if (nameflag)
+        if (nameflag) {
             res += " and ";
+        }
         res += "Address should contain less than 10 characters";
-
     }
-    if (res === "")
+    if (res === "") {
         res = "Hellow " + document.querySelector("#name").value;
+    }
+    document.querySelector("#name").value = "";
+    document.querySelector("#address").value = "";
     window.alert(res);
 }
